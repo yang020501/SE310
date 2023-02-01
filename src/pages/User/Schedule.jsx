b3e042e2913eae7051146de3c460276b78d10d9d
yang020501 <thaiduong020501@gmail.com>
2023-02-01 12:33:57 +0700

    Update
    update

---
+++ src/pages/User/Schedule.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Timetable from '../../components/Timetable'
+
+const Schedule = () => {
+  return (
+    <Timetable>
+        
+    </Timetable>
+  )
+}
+
+export default Schedule
